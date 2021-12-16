a48b431b7d9e84286bfbca5756182dd44a86f5dc
Louie Bond <louiebond@yahoo.com>
2021-12-16 09:10:05 +0000

    added comment

--- app/errors.py
+++ app/errors.py
@@ -1,6 +1,8 @@
 from flask import render_template
 from app import app, db
 
+#same as routes, but returns error changes
+
 @app.errorhandler(404)
 def not_found_error(error):
     return render_template('404.html'), 404
